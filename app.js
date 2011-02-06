8721a8fd82d3f7f89de3668a59b02a00d419734b
Yaroslav Shirokov <Yaroslav.Shirokov@cmgdigital.com>
2011-02-06 17:04:09 -0500

    [master] I thin the oauth lib is busted :O

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ var opts = require('tav').set(
     {repl: {
          note: 'Open a REPL into the app',
          value: false
-     }},
+     },
+     consumer_key: {
+         note: "Twitter consumer key"
+     },
+     consumer_secret: {
+         note: "Twitter secret key"
+     },
+     access_token_key: {
+         note: "Twitter access key"
+     },
+     access_token_secret: {
+         note: "Twitter secret key"
+     }
+    },
     "Usage: {self} [options]".$format({self: require('helpers').proc.$0()}),
     true // Panic on unknown options
 );
