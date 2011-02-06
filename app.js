82f9f8f7b521da9d8498ffcf1a4399ae112f8d8a
Yaroslav Shirokov <Yaroslav.Shirokov@cmgdigital.com>
2011-02-05 23:51:02 -0500

    [master] Formatting my own name and some crap I don't remember. Early in the history, deal with it.

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
 (function() {require('./lib');})(); //Loader scaffold
-var twitter = require('twitter-node');
+var twitter = require('twitter-node'),
+    opts = require('tav');
 
+opts = opts.set(
+    {repl: {
+         note: 'Open a REPL into the app',
+         value: false
+     }},
+    "Usage: {self} [options]".$format({self: require('helpers').proc.$0()}),
+    true // Panic on unknown options
+);
 
+console.log("Loading with: ", opts, opts.args);
 //REPLs
-(function() {
+(function () {
      var net = require('net'),
          repl = require('repl');
-     var prompt = 'poopvolcano> ';
+     var sock = __dirname + "/repl.sock",
+         prompt = 'poopvolcano> ';
+     var server = function(fn) { return server = net.createServer(fn); };
 
-     repl.start(prompt);
-
-     net.createServer(function (socket) {
-                          repl.start(prompt, socket);
-                      }
-     ).listen(__dirname + "/repl.sock");
-})();
+     server(function (socket) {
+                repl.start(prompt, socket);
+         }).
+         listen(__dirname + "/repl.sock");
+});
